12d659622dc8157c37bfa1e9ad6a335e5cb52f4b
Aagaman Varma <aagaman32@gmail.com>
2025-07-18 18:48:10 +0545

    added clean_markdown for cleaner response

--- app.py
+++ app.py
@@ -1,93 +1,10 @@
-# from flask import Flask, render_template, request
-# import fitz
-# import os
-# import spacy
-# from spacy.matcher import PhraseMatcher
-
-# app = Flask(__name__)
-# UPLOAD_FOLDER = 'uploads'
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# if not os.path.exists(UPLOAD_FOLDER):
-#     os.makedirs(UPLOAD_FOLDER)
-
-# nlp = spacy.load("en_core_web_sm")
-
-# skills_list = [
-#     "Python", "Java", "C++", "SQL", "Machine Learning", "Data Analysis",
-#     "Communication", "Teamwork", "Project Management", "AWS", "Docker",
-#     "Flask", "Django", "JavaScript", "React", "HTML", "CSS"
-# ]
-
-# matcher = PhraseMatcher(nlp.vocab)
-# patterns = [nlp(skill) for skill in skills_list]
-# matcher.add("SKILLS", None, *patterns)
-
-# def analyze_resume(text):
-#     doc = nlp(text)
-#     entities = [(ent.text, ent.label_) for ent in doc.ents]
-
-#     matches = matcher(doc)
-#     skills = set()
-#     for match_id, start, end in matches:
-#         span = doc[start:end]
-#         skills.add(span.text)
-
-#     experience_sentences = [sent.text for sent in doc.sents if "experience" in sent.text.lower()]
-
-#     return {
-#         "entities": entities,
-#         "skills": sorted(skills),
-#         "experience_summary": experience_sentences
-#     }
-
-# def extract_text_from_pdf(pdf_path):
-#     text = ""
-#     with fitz.open(pdf_path) as doc:
-#         for page in doc:
-#             text += page.get_text()
-#     return text
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# from gemini_suggester import get_resume_feedback  # Make sure this file exists!
-
-# @app.route('/upload', methods=['POST'])
-# def upload():
-#     if 'resume' not in request.files:
-#         return "No file part", 400
-
-#     file = request.files['resume']
-#     if file.filename == '':
-#         return "No selected file", 400
-
-#     filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-#     file.save(filepath)
-
-#     text = extract_text_from_pdf(filepath)
-#     analysis = analyze_resume(text)
-
-#     # Get Gemini suggestions
-#     suggestions = get_resume_feedback(text)
-
-#     return render_template("result.html", analysis=analysis, suggestions=suggestions)
-
-
-
-
- 
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
+import re
 from flask import Flask, render_template, request
 import fitz
 import os
 import spacy
 from spacy.matcher import PhraseMatcher
+from gemini_suggester import get_resume_feedback
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
@@ -133,7 +50,12 @@ def extract_text_from_pdf(pdf_path):
             text += page.get_text()
     return text
 
-from gemini_suggester import get_resume_feedback  # Make sure gemini_suggester.py is in the same folder
+def clean_markdown(text):
+    # Remove Markdown headers (#, ##, ###)
+    text = re.sub(r'#+\s*', '', text)
+    # Remove bold and italics marks
+    text = re.sub(r'\*\*|__|\*|_', '', text)
+    return text
 
 @app.route('/')
 def index():
@@ -148,7 +70,6 @@ def upload():
     if file.filename == '':
         return "No selected file", 400
 
-    # Save file safely
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file.save(filepath)
 
@@ -158,9 +79,11 @@ def upload():
         return f"Failed to extract PDF text: {e}", 500
 
     analysis = analyze_resume(text)
-    suggestions = get_resume_feedback(text)
 
-    return render_template("result.html", analysis=analysis, suggestions=suggestions)
+    raw_suggestions = get_resume_feedback(text)
+    cleaned_suggestions = clean_markdown(raw_suggestions)
+
+    return render_template("result.html", analysis=analysis, suggestions=cleaned_suggestions)
 
 if __name__ == '__main__':
     app.run(debug=True)
